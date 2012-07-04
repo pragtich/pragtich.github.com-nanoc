3a632beac2ca14220558a789ea65139d79776613
Joris Pragt <pragtich@github>
2012-07-04 18:18:24 +0200

    some clean up of the master

--- lib/default.rb
+++
@@ -1,8 +0,0 @@
-# All files in the 'lib' directory will be loaded
-# before nanoc starts compiling.
-include Nanoc3::Helpers::Rendering
-include Nanoc3::Helpers::Blogging
-include Nanoc3::Helpers::LinkTo
-include Nanoc3::Helpers::Capturing
-
-$cats = Hash.new {|h, k| h[k] = []}
